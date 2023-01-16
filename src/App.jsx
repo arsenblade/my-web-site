2adcb2dd5a54583b4ad10f664f47c7b6d5664a8c
Arsen <arsenblade@mail.ru>
2023-01-16 20:56:18 +0500

    Создание header и настройка основных стилей

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
@@ -12,6 +13,7 @@ const App = () => {
       <Header />
       <Nav />
       <About />
+      <Experience />
       <Portfolio />
       <Contact />
       <Footer />
